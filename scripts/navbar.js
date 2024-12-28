4d350cea8055fb6560e8be436a3d3d8a98cea6d8
fowziya01 <fowzijunaid@gmail.com>
2024-12-28 14:44:11 +0530

    common navbar created

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+ const nav=` <h1>Book Management App</h1>
+        <div id="nav-links">
+            <a href="index.html">Home</a>
+            <a href="admin.html">Admin</a>
+            <a href="books.html">Books</a>
+
+        </div>`;
+  document.getElementById("nav").innerHTML=nav;
